39bf371239ef1cb4a645c881f6e8c932bd599123
Daniel Amzovski <daniel@amzovski.de>
2025-01-01 21:02:52 +0100

    * docs + discountReason

--- src/lib/classes/invoice-item-with-discount.ts
+++ src/lib/classes/invoice-item-with-discount.ts
@@ -2,8 +2,21 @@ import {DiscountType} from "../enums/discount.enum";
 import {InvoiceItemWithoutDiscount} from "./invoice-item-without-discount";
 
 export class InvoiceItemWithDiscount extends InvoiceItemWithoutDiscount {
+    /**
+     * The discount amount
+     */
     discount: number = 0;
+    /**
+     * The type of the discount
+     * @default DiscountType.FIXED
+     * @see DiscountType
+     */
     discountType: DiscountType = DiscountType.FIXED;
+    /**
+     * The reason for the discount
+     */
+    discountReason: string = '';
+
 
     get discountAmount(): number {
         if (this.discountType === DiscountType.PERCENTAGE) {
